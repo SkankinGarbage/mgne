2189ee235341c6d01e90d7ff8318e78aa9bb69bc
aking <psy_wombats@wombatrpgs.net>
2015-05-25 21:56:51 -0700

    Eventing out the wolf's castle

---
+++ saga/res/scenes/world2/steward.lua
@@ -0,0 +1,15 @@
+local hero1 = getHero(0).getName()
+local hero2 = getHero(1).getName()
+
+speak("Lara", "Where is the wolf?")
+speak("Steward", "The world wolf is not here. But I will slay you in his name, traitors!")
+
+battle('party_bossShiva', false)
+sceneSwitch('steward_dead', true)
+wait(0.7)
+
+speak(hero2, "Then Jonas really must be held by the worm.")
+speak("Lara", "I'll come with you to the worm's castle, if you can find a way.")
+speak("Lara", "Jonas was working for my organization when he was captured. We believe that the wolf and worm may be from a hidden world called Babel.")
+speak(hero1, "Babel? We've been there, but what is it? What are you and Jonas up to?")
+speak("Lara", "Speak no more! We have work to do.")
