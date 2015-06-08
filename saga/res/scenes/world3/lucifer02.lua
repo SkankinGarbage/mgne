a4d11a37760653ce6d9cbd41cee5bd23f02aff8c
aking <psy_wombats@wombatrpgs.net>
2015-06-07 21:03:21 -0700

    Most of the eventing and sprites for the city

---
+++ saga/res/scenes/world3/lucifer02.lua
@@ -0,0 +1,5 @@
+local hero1 = getHero(0).getName()
+
+speak("Prince", "Welcome to the City of Angels, travellers. Is there anything I can help you with?")
+speak(hero1, "Nothing in particular.")
+speak("Prince", "I hope you'll attend the gate-opening ceremony. My scientists tell me we are very close!")
