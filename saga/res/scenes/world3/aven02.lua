905ee30455e6ea8bb1e6f1faa1fab50cffcfccfa
aking <psy_wombats@wombatrpgs.net>
2015-06-10 01:23:44 -0700

    Untested stuff for the world 3 rebel base

---
+++ saga/res/scenes/world3/aven02.lua
@@ -0,0 +1,9 @@
+local hero1 = getHero(0).getName()
+local hero2 = getHero(3).getName()
+
+speak("Aven", "You say you are from the same world as Jonas?")
+speak(hero1, "Yes.")
+speak("Aven", "Then maybe you remember a seal on the gate of Hero?")
+speak("Aven", "That was my work.")
+speak(hero2, "Of course, the mirror!")
+speak(hero1, "Best not mention it...")
