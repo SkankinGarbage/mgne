4a54a2eb15ff57fb27c01605d92321e77c96f0b7
aking <psy_wombats@wombatrpgs.net>
2015-10-19 00:47:02 -0700

    World 5 brotherhood cutscene

---
+++ saga/res/scenes/world5/brotherhood01.lua
@@ -0,0 +1,28 @@
+local hero1 = getHero(0).getName()
+local hero2 = getHero(1).getName()
+
+sceneSwitch('brotherhood_appears', true)
+
+walk('zkauba', 10, 'WEST', false)
+walk('lara', 10, 'WEST', true)
+walk('zkauba', 2, 'WEST', false)
+walk('lara', 2, 'NORTH', true)
+walk('zkauba', 1, 'NORTH', true)
+
+speak(hero2, "Look, it's Zkauba and Lara!")
+speak("Lara", hero1 .. ", you're -")
+speak("Zkauba", "We were worried! Jonas dead, the door to Babel's heartland opened...")
+speak("Janine", "Jonas...")
+speak("Lara", "We buried him in the foothills. Say, aren't you -")
+speak("Zkauba", "His treacherous twin! You're lucky I don't kill you where you stand!")
+speak(hero1, "Jonas died to protect her.")
+speak("Janine", "And I'll avenge him. Aven betrayed us.")
+speak("Lara", "We thought so... He tried to trap us in Babel, and -")
+speak("Zkauba", "And Lara almost died in the ambush. We'll try to help you if we still can.")
+speak("Lara", "Aven may have left the brotherhood but we'll still fight for its ideals. No more masters!")
+
+walk('lara', 2, 'NORTH', false)
+walk('zkauba', 2, 'NORTH', true)
+
+sceneSwitch('brotherhood_appears', false)
+sceneSwitch('brotherhood01', true)
---
+++ saga/res/scenes/world5/lara.lua
@@ -0,0 +1,7 @@
+local hero1 = getHero(0).getName()
+local hero2 = getHero(1).getName()
+
+speak("Lara", "So this is the heart of Babel. It's peaceful here.")
+speak(hero1, "What do you know about Babel?")
+speak("Lara", "Only what Aven taught us.")
+speak("Lara", "Babel is network to connect the creations of the masters. No doubt Aven wants to be a master himself.")
