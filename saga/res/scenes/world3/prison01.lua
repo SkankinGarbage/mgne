5b6a34584274c57d6b836a15e9062de3c5d576ec
aking <psy_wombats@wombatrpgs.net>
2016-01-17 13:33:09 -0800

    Redo of the prison cutscene

--- saga/res/scenes/world3/prison01.lua
+++ saga/res/scenes/world3/prison01.lua
@@ -1,12 +1,26 @@
 local hero1 = getHero(0).getName()
 
-speak("Man", "No way you can bend those bars. They're controlled electronically by the top guys that the prince trusts.")
-
+speak("Zkauba", "Hrrrrgh!")
+speak("Man", "No way you can bend those bars.")
 wait(2.0)
+
+walk('janine', 2, 'SOUTH')
+walk('janine', 2, 'EAST')
+face('janine', 'SOUTH')
+wait(0.8)
+walk('janine', 1, 'EAST')
+wait(0.4)
+
 playSound('explode')
-shake(.3)
-sceneSwitch('prison01', true)
-wait(.5)
-speak("Man", "Wow! Someone big must like you!")
+shake(0.3)
+sceneSwitch('prison_lever', true)
+wait(0.5)
+
+walk('janine', 3, 'WEST')
+walk('janine', 2, 'NORTH')
+
+speak("Man", "Wow! Who was that?")
 speak(hero1, "...Thank you Janine.")
 speak("Zkauba", "Hmph.")
+
+sceneSwitch('prison01', true)
