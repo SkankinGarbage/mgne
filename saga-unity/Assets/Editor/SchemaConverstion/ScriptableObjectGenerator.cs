f7d47c22618c7635fe1c8cda98579baca3f5d118
psywombats <psy_wombats@wombatrpgs.net>
2020-01-06 00:22:52 -0500

    Pausing the DB work

--- saga-unity/Assets/Editor/SchemaConverstion/ScriptableObjectGenerator.cs
+++ saga-unity/Assets/Editor/SchemaConverstion/ScriptableObjectGenerator.cs
@@ -4,15 +4,18 @@ using UnityEditor;
 using System.IO;
 using System;
 using Newtonsoft.Json;
+using System.Reflection;
 
 public class ScriptableObjectGenerator {
 
-    private const string DataDirectory = "DataJsonTest";
+    public const string DataDirectory = "DataJsonTest";
+    public const string DatabaseDirectory = "Resources/Database";
 
     [MenuItem("MGNE Tools/Convert Json")]
     public static void ConvertJson() {
         var directoryPath = Application.dataPath + "/" + DataDirectory;
         ConvertFromDirectory(directoryPath);
+        AssetDatabase.SaveAssets();
     }
 
     private static void ConvertFromDirectory(string directoryPath) {
@@ -23,38 +26,61 @@ public class ScriptableObjectGenerator {
             ConvertFromDirectory(subdirectoryPath);
         }
     }
-    
+
     private static void ConvertFromFile(string filePath) {
         if (!filePath.EndsWith(".json")) {
             return;
         }
 
-        FileStream fileReader = File.Open(filePath, FileMode.Open);
-        StreamReader streamReader = new StreamReader(fileReader);
-        string jsonString = streamReader.ReadToEnd();
-        streamReader.Close();
-        fileReader.Close();
+        var jsonString = JsonAtFilepath(filePath);
+        var type = TypeForFilepath(filePath);
+        if (type != null) {
+            ConvertRaw(type, jsonString);
+            //Link(type, jsonString);
+        }
+    }
 
+    private static void ConvertRaw(Type type, string jsonString) {
+        var obj = JsonConvert.DeserializeObject(jsonString, type, new LinkIgnoreDeserializer());
+        var path = PathForJson(type, obj as MainSchema);
+
+        if (!File.Exists(path)) {
+            AssetDatabase.CreateAsset((UnityEngine.Object)obj, path);
+            AssetDatabase.SaveAssets();
+            Debug.Log("Writing to " + path + "...");
+        }
+    }
+
+    private static void Link(Type type, string jsonString) {
+        var linkedAsset = (MainSchema) JsonConvert.DeserializeObject(jsonString, type, new LinkerDeserializer());
+        var storedAsset = AssetForJson(type, linkedAsset);
+
+        // copy from the linked version to the one on disk
+        PropertyInfo[] destinationProperties = storedAsset.GetType().GetProperties();
+        foreach (PropertyInfo destinationPi in destinationProperties) {
+            PropertyInfo sourcePi = linkedAsset.GetType().GetProperty(destinationPi.Name);
+            destinationPi.SetValue(storedAsset, sourcePi.GetValue(linkedAsset, null), null);
+        }
+    }
+
+    public static Type TypeForFilepath(string filePath) {
         string typeString = filePath.Substring(0, filePath.LastIndexOf('\\'));
         typeString = typeString.Substring(typeString.LastIndexOf('\\') + 1, typeString.Length - typeString.LastIndexOf('\\') - 1);
         typeString = typeString.Replace("MDO", "Data");
         var assembly = typeof(CharaData).Assembly;
         Type type = assembly.GetType(typeString);
-        if (type != null) {
-            ConvertRaw(type, jsonString);
-        }
+        return type;
     }
 
-    private static void ConvertRaw(Type type, string jsonString) {
-        var obj = JsonConvert.DeserializeObject(jsonString, type);
-        var path = "Assets/Resources/Database/" + type.ToString();
+    public static string PathForJson(Type schemaSubclass, MainSchema mainSchema) {
+        var path = "Assets/Resources/Database/" + schemaSubclass.ToString();
         path = path.Substring(0, path.LastIndexOf("Data"));
         if (!Directory.Exists(path)) {
             Directory.CreateDirectory(path);
         }
-        var mainSchema = obj as MainSchema;
         if (mainSchema == null) {
-            Debug.LogError("Bad schema " + type);
+            Debug.LogError("Bad schema " + schemaSubclass);
+            return "";
         }
         if (mainSchema.subfolder.Length > 0) {
             path += "/" + mainSchema.subfolder;
@@ -63,10 +89,43 @@ public class ScriptableObjectGenerator {
             }
         }
         path += "/" + mainSchema.key + ".asset";
-        
-        if (!File.Exists(path)) {
-            AssetDatabase.CreateAsset((UnityEngine.Object) obj, path);
-            Debug.Log("Writing to " + path + "...");
+        return path;
+    }
+
+    public static string JsonAtFilepath(string filepath) {
+        FileStream fileReader = File.Open(filepath, FileMode.Open);
+        StreamReader streamReader = new StreamReader(fileReader);
+        string jsonString = streamReader.ReadToEnd();
+        streamReader.Close();
+        fileReader.Close();
+
+        return jsonString;
+    }
+
+    public static MainSchema AssetForJson(Type schemaSubclass, MainSchema mainSchema) {
+        string path = DatabaseDirectory + "/" + schemaSubclass.ToString();
+        if (mainSchema.subfolder?.Length > 0) {
+            path += "/" + mainSchema.subfolder;
         }
+        path += mainSchema.key + ".asset";
+        return AssetDatabase.LoadAssetAtPath<MainSchema>(path);
+    }
+
+    public static MainSchema FindAssetForJson(Type schemaSubclass, string key) {
+        string path = Application.dataPath + DatabaseDirectory + "/" + schemaSubclass.ToString();
+        var targetName = key + ".asset";
+        foreach (string filePath in Directory.EnumerateFiles(path)) {
+            if (filePath.EndsWith(targetName)) {
+                AssetDatabase.LoadAssetAtPath<MainSchema>(filePath);
+            }
+        }
+        foreach (string directoryPath in Directory.EnumerateDirectories(path)) {
+            foreach (string filePath in Directory.EnumerateFiles(path)) {
+                if (filePath.EndsWith(targetName)) {
+                    AssetDatabase.LoadAssetAtPath<MainSchema>(filePath);
+                }
+            }
+        }
+        return null;
     }
 }
--- saga-unity/Assets/Scripts/Data/Schema/AbilEffectData.cs
+++ saga-unity/Assets/Scripts/Data/Schema/AbilEffectData.cs
@@ -1,3 +1,3 @@
-public class AbilEffectData : MainSchema {
+public class AbilEffectData : PolymorphicSchema {
 
 }
